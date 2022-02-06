faa1efdf6486e4a9fdfd6dca4fd89c1793a744ed
Jerry Phan <jerryphan1@gmail.com>
2022-02-06 11:40:51 -0800

    heroku push

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
-module.exports = {
-  mongoURI: 'mongodb+srv://jerryphan:DUWLzTJ4SL8gowFs@cluster0.txcdp.mongodb.net/Cluster0?retryWrites=true',
-  secretOrKey: "superdupersecretsecretkey"
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
-
---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb+srv://jerryphan:DUWLzTJ4SL8gowFs@cluster0.txcdp.mongodb.net/Cluster0?retryWrites=true',
+  secretOrKey: "superdupersecretsecretkey"
+}
+
